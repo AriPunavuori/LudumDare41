a16b90f2a67dde63c498590acce5c49eefd92419
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-04-21 14:44:40 +0300

    no message

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float movingSensitivity;
+	
+	void Update () {
+        //if (Input.GetKeyDown(KeyCode.))
+		
+	}
+}
