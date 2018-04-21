faba0be94e48b604e2308fdc63ae5ed342f96f74
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-04-21 15:36:16 +0300

    xd
    Pallon liikkuminen.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
     public float movingSensitivity;
+    public float Speed;
 	
 	void Update () {
-        //if (Input.GetKeyDown(KeyCode.))
-		
-	}
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+                transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+        }
+    }
 }
