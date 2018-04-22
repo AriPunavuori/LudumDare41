8bf7ec9648d7ab487a87d826adbfae6efeb86ade
sara <sokeritahra@gmail.com>
2018-04-22 12:22:23 +0300

    Keilarypäs ja kaato

--- Assets/Scripts/BowlTrigger.cs
+++ Assets/Scripts/BowlTrigger.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BowlTrigger : MonoBehaviour {
 
     Rigidbody rb;
     public Vector3 MForce;
-
+    Quaternion startRot;
+    bool knockedOver;
+    PinsCount pinsCount;
    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startRot = transform.rotation;
+        pinsCount = GetComponentInParent<PinsCount>();
     }
 
     void Update () {
-		
+        //print(Mathf.Abs(transform.rotation.x - startRot.x));
+        //print(Mathf.Abs(transform.rotation.z - startRot.z));
+		if (Mathf.Abs( transform.rotation.x - startRot.x ) > 0.1 && !knockedOver || 
+                Mathf.Abs( transform.rotation.z - startRot.z) > 0.1 && !knockedOver) {
+            print("keila kaatu");
+            pinsCount.PinFell();
+            knockedOver = true;
+        }
     
 	}
 
@@ -24,6 +36,8 @@ public class BowlTrigger : MonoBehaviour {
         {
             rb.constraints = RigidbodyConstraints.None;
             rb.AddForce(MForce, ForceMode.Impulse);
+            print("osu");
+
             //Destroy(gameObject);
         }
     }
