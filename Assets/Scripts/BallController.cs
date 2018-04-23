03b2e8f49487f05507dcaf18dc831e4995f954c8
OPISTO\Joni.Berg <berg.joni.petteri@gmail.com>
2018-04-23 09:28:54 +0300

    Menu
    Menu Scene peliin.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -18,5 +18,26 @@ public class BallController : MonoBehaviour {
         if (Input.GetKey(KeyCode.LeftArrow)) {
             rb.AddForce(cam.right * -1 * sidewaysAcceleration * Time.deltaTime, ForceMode.Acceleration);
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+           GamePause();
+
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void GamePause()
+    {
+
+        Time.timeScale = 0;
+        print("Tauko peliin!");
+
+       
+
     }
 }
